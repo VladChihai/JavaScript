ab603368a3eefd902aa6bd52d138dc910b03bec6
VladChihai <chihaivlad0@gmail.com>
2025-02-16 11:14:18 +0200

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,14 @@
+let hour = 11;
+
+if (hour <= 6){
+    console.log("Buna Dimineata!");
+
+}else if (hour <= 12){
+    console.log("Buna Dimineata!")
+}else if (hour <= 18){
+    console.log("Buna Ziua")
+}else if (hour <= 23.59){
+    console.log("Buna Seara")
+}
+
+console.log("Enjoy")
